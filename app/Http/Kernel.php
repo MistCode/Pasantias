2d18c5f3d431c2f5d5004c7bedece5247498657b
Arturo <arturovielma41@gmail.com>
2019-10-09 18:52:25 -0430

    Creada las rutas con el middleware de auth y permission, ya todas las rutas menos, el registrarse, loguearse, editar el usuario y el index no tienen los permisos

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,8 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'has.role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
         'has.permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
+        'role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,49 +18,32 @@
 
 Route::get('/', 'Control@index');
 
+/*
 //Rutas de Grupo
-
 Route::get('/grupos/create','PaginaController@create')->name('grupos.create');
-
 Route::post('/grupos/create','PaginaController@store');
-
 Route::get('/grupos/{$slug}', 'PaginaController@show')->name('grupos.show');
-
 Route::resource('grupos','PaginaController');
 
 //route Comunidad
-
 Route::resource('comunidades', 'ComunidadController');
-
 Route::get('/comunidades/create','ComunidadController@create')->name('comunidades.create');
-
 Route::post('/comunidades/create','ComunidadController@store');
-
 Route::get('/comunidades/{$slug}', 'ComunidadController@show')->name('comunidades.show');
-
+*/
 //route Persona
-
 Route::resource('personas','PersonaController');
-
 Route::get('/grupos/{grupo}/personas','PersonaController@index');
-
 Route::post('/grupos/{grupo}/personas','PersonaController@store');
-
 Route::put('/grupos/{grupo}/personas/{id}', 'PersonaController@update');
-
 Route::delete('/grupos/{grupo}/personas/{id}', 'PersonaController@destroy');
 
 //route info
 Route::resource('info', 'MensajeController');
-
 Route::get('/comunidades/{comunidad}/info','MensajeController@index');
-
 Route::post('/comunidades/{comunidad}/info','MensajeController@store');
-
 Route::put('/comunidades/{comunidad}/info/{id}','MensajeController@update');
-
 Route::delete('/comunidades/{comunidad}/info/{id}','MensajeController@destroy');
-
 
 Auth::routes();
 
@@ -69,3 +52,66 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/profile', 'UserController@profile')->name('user.profile');
 
 Route::patch('/profile', 'UserController@update_profile')->name('user.profile.update');
+
+Route::middleware(['auth'])->group(function () {
+	//Roles
+	Route::post('roles/store', 'RoleController@store')->name('roles.store')
+		->middleware('permission:roles.create');
+	Route::get('roles', 'RoleController@index')->name('roles.index')
+		->middleware('permission:roles.index');
+	Route::get('roles/create', 'RoleController@create')->name('roles.create')
+		->middleware('permission:roles.create');
+	Route::put('roles/{role}', 'RoleController@update')->name('roles.update')
+		->middleware('permission:roles.edit');
+	Route::get('roles/{role}', 'RoleController@show')->name('roles.show')
+		->middleware('permission:roles.show');
+	Route::delete('roles/{role}', 'RoleController@destroy')->name('roles.destroy')
+		->middleware('permission:roles.destroy');
+	Route::get('roles/{role}/edit', 'RoleController@edit')->name('roles.edit')
+		->middleware('permission:roles.edit');
+
+	//Users
+	Route::get('users', 'UserController@index')->name('users.index')
+		->middleware('permission:users.index');
+	Route::put('users/{user}', 'UserController@update')->name('users.update')
+		->middleware('permission:users.edit');
+	Route::get('users/{user}', 'UserController@show')->name('users.show')
+		->middleware('permission:users.show');
+	Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy')
+		->middleware('permission:users.destroy');
+	Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit')
+		->middleware('permission:users.edit');
+		/**/
+	//Grupos
+	Route::post('grupos/store', 'PaginaController@store')->name('grupos.store')
+		->middleware('permission:grupos.create');
+	Route::get('grupos', 'PaginaController@index')->name('grupos.index')
+		->middleware('permission:grupos.index');
+	Route::get('grupos/create', 'PaginaController@create')->name('grupos.create')
+		->middleware('permission:grupos.create');
+	Route::put('grupos/{$slug}', 'PaginaController@update')->name('grupos.update')
+		->middleware('permission:grupos.edit');
+	Route::get('grupos/{$slug}', 'PaginaController@show')->name('grupos.show')
+		->middleware('permission:grupos.show');
+	Route::delete('grupos/{$slug}', 'PaginaController@destroy')->name('grupos.destroy')
+		->middleware('permission:grupos.destroy');
+	Route::get('grupos/{$slug}/edit', 'PaginaController@edit')->name('grupos.edit')
+		->middleware('permission:grupos.edit');
+
+	//Comunidades
+	Route::post('comunidades/store', 'ComunidadController@store')->name('comunidades.store')
+		->middleware('permission:comunidades.create');
+	Route::get('comunidades', 'ComunidadController@index')->name('comunidades.index')
+		->middleware('permission:comunidades.index');
+	Route::get('comunidades/create', 'ComunidadController@create')->name('comunidades.create')
+		->middleware('permission:comunidades.create');
+	Route::put('comunidades/{$slug}', 'ComunidadController@update')->name('comunidades.update')
+		->middleware('permission:comunidades.edit');
+	Route::get('comunidades/{$slug}', 'ComunidadController@show')->name('comunidades.show')
+		->middleware('permission:comunidades.show');
+	Route::delete('comunidades/{$slug}', 'ComunidadController@destroy')->name('comunidades.destroy')
+		->middleware('permission:comunidades.destroy');
+	Route::get('comunidades/{$slug}/edit', 'ComunidadController@edit')->name('comunidades.edit')
+		->middleware('permission:comunidades.edit');
+
+});
